d869281925833f9ab728a48787a069b5260a02d0
Federico Nardini <fnardini1996@gmail.com>
2022-10-13 17:40:41 +0200

    add Unsplash img & styled index w/ bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 100; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:
+                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, similique dolor. Possimus quia recusandae eos repellat voluptates, omnis enim, quasi asperiores atque earum ea eveniet distinctio est suscipit obcaecati vita",
+            price,
         });
         await camp.save();
     }
